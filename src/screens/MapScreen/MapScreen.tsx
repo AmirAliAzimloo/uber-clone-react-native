d0ae9394e488ca77e84bce21db4725f0077d3f40
AmirAliAzimloo <amiraliazimloo123@gmail.com>
2024-04-06 01:15:41 +0430

    Add pakages.

---
+++ src/screens/MapScreen/MapScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+
+import {StyledMapView, Container} from './MapScreen.styles';
+
+const MapScreen = () => {
+  return (
+    <StyledMapView />
+  )
+}
+ 
+export default MapScreen
